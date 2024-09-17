474afe2a4d562f30cbc37b5aa12426375de56e0d
DESKTOP-300B3DB\Techovirish Boopathi <elavarasan.p@techovirish.com>
2024-09-17 20:36:47 +0530

    initial Commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 
-
 $(document).ready(function() {
 
 
@@ -24,53 +23,39 @@ $(document).ready(function() {
 
 
 //slider
-const $wrapper = $('.clients_wrapper');
-const $slides = $wrapper.children('span');
-const slideCount = $slides.length;
-const slideWidth = $slides.outerWidth(true);
-const slideDuration = 3000; 
-let currentIndex = 0;
+var $clientsWrapper = $('.clients_wrapper');
+var $clients = $clientsWrapper.children().clone();
+$clientsWrapper.append($clients);
+var totalWidth = $clientsWrapper.children().length * $clientsWrapper.children().first().outerWidth(true);
+$('.clients_wrapper').css('width',`100%`); 
+$('.clients_wrapper').css('animation-duration', '15s'); 
 
 
-$wrapper.append($slides.first().clone());
 
-function moveSlide() {
-  $wrapper.css('transform', 'translateX(' + (-currentIndex * slideWidth) + 'px)');
-  currentIndex++;
-  if (currentIndex >= slideCount) {
-    
-    setTimeout(function() {
-      $wrapper.css('transition', 'none');
-      $wrapper.css('transform', 'translateX(0)');
-      currentIndex = 0;
-      setTimeout(function() {
-        $wrapper.css('transition', 'transform 0.5s ease');
-      }, 20); 
-    }, slideDuration);
-  }
-}
-
-setInterval(moveSlide, slideDuration);
-
-
-  $('.clients_wrapper span').click(function() {
-    var clientInfo = $(this).data('client-info');
-    $('.popup-text').text(clientInfo);
-    $('.popup-overlay').fadeIn();
+//tooltip client section
+   // When a client logo is clicked
+   $('.client_logo').click(function() {
+    var info = $(this).parent().attr('data-info'); 
+    $('#modalText').text(info); 
+    $('#infoModal').show(); 
   });
 
 
-  $('.popup-close, .popup-overlay').click(function() {
-    $('.popup-overlay').fadeOut();
+  $('.close').click(function() {
+    $('#infoModal').hide(); 
   });
+
+
+  $(window).click(function(event) {
+    if ($(event.target).is('#infoModal')) {
+      $('#infoModal').hide(); 
+    }
+  });
+
 
   
-  $('.popup-content').click(function(event) {
-    event.stopPropagation();
-  });
-
-
-
 });
+ 
+
 
 
